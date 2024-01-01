a0461be070c00ddd544f4a95cfc052b13910aef1
Julia Wang <90004776+jlwang03@users.noreply.github.com>
2023-12-31 18:48:27 -0800

    scrolling background with new bg size assets

--- scripts/bg-endless-scrolling.gd
+++ scripts/bg-endless-scrolling.gd
@@ -1,20 +1,7 @@
-extends Sprite
+extends ParallaxBackground
 
+const SPEED: float = 5.0
+const SCROLLING_MULTIPLIER: float = 100.0
 
-const SPEED: float = -500.0
-var texture_width: float = 0
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	texture_width = texture.get_size().x * scale.x
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.x += SPEED * delta
-	_attempt_reposition()
-
-func _attempt_reposition() -> void:
-	if position.x < -texture_width:
-		position.x += (2 * texture_width - 50.0)
+func _process(delta):
+	self.scroll_base_offset -= (Vector2(SPEED * SCROLLING_MULTIPLIER, 0) * delta)
