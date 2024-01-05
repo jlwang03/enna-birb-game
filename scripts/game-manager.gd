55d9d6c8d39b5acbe5e75ca97027f2df4a97b0a5
Julia Wang <90004776+jlwang03@users.noreply.github.com>
2024-01-05 01:53:03 -0800

    basic scoring

--- scripts/game-manager.gd
+++ scripts/game-manager.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 
 const SPEED_MULTIPLIER: float = 1.0
-const SCORE_INCREASE_COOLDOWN: float = 1.0
+const SCORE_INCREASE_COOLDOWN: float = 0.1
 
 onready var _ui_pause
 onready var _ui_hearts
+onready var _ui_score
 
 var _score
 var _health
@@ -17,19 +18,21 @@ func _init():
 	_score = 0
 	_health = 2
 	_speed = 500.0
-	_score_delta_time = SCORE_INCREASE_COOLDOWN
+	_score_delta_time = 0
 
 func _ready():
 	_ui_pause = $HUD/Pause
 	_ui_hearts = [$HUD/Hearts/heart1, $HUD/Hearts/heart2, $HUD/Hearts/heart3]
+	_ui_score = $HUD/Score
 	
 	update_hearts_ui()
 	
 func _process(delta: float):
 	# Increase score
 	_score_delta_time -= delta
-	if _score_delta_time < 0:
+	if _score_delta_time <= 0:
 		_score += 1
+		_ui_score.text = "Score: %d" % _score
 		_score_delta_time = SCORE_INCREASE_COOLDOWN
 	
 	# Update scrolling speed
@@ -53,3 +56,7 @@ func update_hearts_ui():
 	_ui_hearts[0].visible = (_health >= 1)
 	_ui_hearts[1].visible = (_health >= 2)
 	_ui_hearts[2].visible = (_health >= 3)
+
+
+func _on_Pause_pressed():
+	pass # Replace with function body.
