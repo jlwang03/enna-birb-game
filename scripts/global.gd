a81fb3fbedaa9b0b656c8d2cc4b950455de106c6
Julia Wang <90004776+jlwang03@users.noreply.github.com>
2024-01-09 19:08:21 -0800

    add loading screen into gameplay

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,21 +1,37 @@
 extends Node
 
+const LOADING_SCENE_DURATION: float = 1.5
+
 var _highestScore
 var _inGame: bool
 var _playerColliderID: int
 var _isRunning: bool # Is false when the player pauses the game
 
+var _scene_to_load: String
+var _duration: float
+
 func _init():
 	_highestScore = 0
 	_inGame = false
 	
-	
+	_scene_to_load = ""
+	_duration = 0.0
+
+
+func _process(delta):
+	if _scene_to_load != "":
+		_duration -= delta
+		if _duration <= 0:
+			get_tree().change_scene(_scene_to_load)
+			_scene_to_load = ""
+
+
 func StartGame():
 	_inGame = true
 	_isRunning = true
 	
 	# Load game screen
-	get_tree().change_scene("res://scenes/main-scene.tscn")
+	SetLoading("res://scenes/main-scene.tscn")
 
 
 func GameOver(score):
@@ -26,7 +42,7 @@ func GameOver(score):
 		_highestScore = score
 	
 	# Load start screen
-	get_tree().change_scene("res://scenes/start-scene.tscn")
+	SetLoading("res://scenes/start-scene.tscn")
 
 
 func GetHighscore() -> int:
@@ -39,3 +55,8 @@ func IsRunning() -> bool:
 
 func SetIsRunning(running: bool):
 	_isRunning = running
+
+func SetLoading(scene: String):
+	get_tree().change_scene("res://scenes/loading-scene.tscn")
+	_duration = LOADING_SCENE_DURATION
+	_scene_to_load = scene
