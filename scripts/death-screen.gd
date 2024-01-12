5b8a0736656c39d7a5748f7cff9daa4fbc016f43
Julia Wang <90004776+jlwang03@users.noreply.github.com>
2024-01-12 02:40:44 -0800

    death screen variations

--- scripts/death-screen.gd
+++ scripts/death-screen.gd
@@ -1,11 +1,18 @@
 extends Control
 
+export(Array, Texture) var _texture
+export(Array, Vector2) var _texture_position
+
 
 func OpenScreen(score: int):
+	# Global calls - Stop running and update highest scores
 	Global.SetIsRunning(false)
-	
 	if score > Global._highestScore:
 		Global._highestScore = score
 	
+	# Update UI
+	var i = randi() % _texture.size()
+	$enna.texture = _texture[i]
+	$enna.rect_position = _texture_position[i]
 	$score.text = "Score: %d" % score
 	show()
